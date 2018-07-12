c42e7f71dccde59669e3a039875abe753b4c6d4a
Alanne Soares <alannesilva@yahoo.com.br>
2018-07-11 23:05:33 -0300

    create new branch v.5

--- ReadDataPDFTable/src/com/company/ReadDataPDFTable.java
+++ ReadDataPDFTable/src/com/company/ReadDataPDFTable.java
@@ -1,15 +1,10 @@
 package com.company;
 
-import org.apache.pdfbox.cos.COSDocument;
-import org.apache.pdfbox.pdfparser.PDFParser;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.util.PDFTextStripper;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.Arrays;
-import java.util.List;
-
+import java.io.*;
+import org.apache.pdfbox.cos.*;
+import org.apache.pdfbox.pdfparser.*;
+import org.apache.pdfbox.pdmodel.*;
+import org.apache.pdfbox.util.*;
 
 public class ReadDataPDFTable {
 
@@ -19,62 +14,37 @@ public class ReadDataPDFTable {
         PDDocument pdDoc;
         COSDocument cosDoc;
         PDFTextStripper pdfStripper;
+        String text;
+        String fileName;
+        String[] linhas;
+        File file;
 
-        String text = null;
-        String fileName = "c:/users/alanne.soares/documents/produtividade-juizes-mai-18.pdf";
-        File file = new File(fileName);
-
+        //String fileName = "c:/users/alanne.soares/documents/produtividade-juizes-mai-18.pdf";
+        fileName = "c:/users/alann/documents/produtividade-juizes-mai-18.pdf";
+        file = new File(fileName);
 
         try {
-
-            /*
-
-            Uma forma melhor de extrair texto
-
-
-                PdfReader arquivo = new PdfReader("Arquivo.pdf");
-
-                for (int i = 1; i < 1000; i++ {
-
-	            String texto = PdfTextExtractor.getTextFromPage(arquivo, i++);
-
-             */
-
             parser = new PDFParser(new FileInputStream(file));
             parser.parse();
             cosDoc = parser.getDocument();
             pdfStripper = new PDFTextStripper();
-            pdfStripper.setLineSeparator("\n");
             pdDoc = new PDDocument(cosDoc);
             text = pdfStripper.getText(pdDoc);
+            linhas = text.split("\\r?\\n");
 
-            String[] linhas = text.split("\\r?\\n");
+            for (int j = 0; j < linhas.length; j++) {
 
-            for (int i = 0; i < linhas.length; i++) {
+                if (Character.isDigit(linhas[j].charAt(linhas[j].length() - 1))) {
 
-                if (Character.isDigit(linhas[i].charAt(linhas[i].length() - 1))) {
+                    String conteudo = Remove.words(Remove.months(Remove.numbers(Remove.specialCharacteres(linhas[j]))));
 
-                    String removeCaracterEspecial = CaracterEspecial.deleteCaracter(linhas[i]).replaceAll("[0-9]", "");
-                    String removeNumbers = Remove.numbers(removeCaracterEspecial);
-                    String removePalavras = Words.removeWords(removeNumbers).replace("  ", "");
-                    String removeMes = Mes.deletePalavra(removePalavras.trim());
-
-                    if (!removeMes.isEmpty()) {
-
-                        System.out.println(String.format("%d: %s", i + 1, removeMes));
+                    if (!conteudo.trim().equals("")) {
+                        System.out.println(String.format(conteudo));
                     }
-
-
                 }
-
             }
-                pdDoc.close();
-
-
         } catch (Exception e) {
-
             System.out.println("Tente novamente!");
         }
-
     }
 }
--- ReadDataPDFTable/src/com/company/Remove.java
+++ ReadDataPDFTable/src/com/company/Remove.java
@@ -1,10 +1,62 @@
 package com.company;
 
 public class Remove {
-    static String numbers(String pdf) {
+
+    public static String numbers(String pdf) {
 
         pdf = pdf.replaceAll("[0-9]", "");
 
         return pdf;
+    }
+
+    public static String months(String pdf) {
+
+        pdf = pdf.replace("Janeiro", "")
+                .replace("Fevereiro", "")
+                .replace("Março", "")
+                .replace("Abril", "")
+                .replace("Maio", "")
+                .replace("Junho", "")
+                .replace("Julho", "")
+                .replace("Agosto", "")
+                .replace("Setembro", "")
+                .replace("Outubro", "")
+                .replace("Novembro", "")
+                .replace("Dezembro", "");
+
+        return pdf;
+    }
+
+    public static String specialCharacteres(String pdf) {
+
+        pdf = pdf.replace(".", "")
+                .replace(",", "")
+                .replace(";", "")
+                .replace("ª", "")
+                .replace("/", "")
+                .replace("|", "")
+                .replace("(", "")
+                .replace(")", "")
+                .replace("-", " ")
+                .replace(":", "")
+                .replace("%", "")
+                .replace("'", "")
+                .replace("[", "")
+                .replace("  ", "")
+                .replace("´", "")
+                .replace("!", "")
+                .replace("^", "");
+
+        return pdf;
+    }
+
+    public static String words(String pdf) {
+
+        pdf = pdf
+                .replace("Produtividade", "")
+                .replace("de", "")
+                .replace("Juízes", "");
+
+            return pdf;
     }
 }
