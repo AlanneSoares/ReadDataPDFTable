d768bed7ff1939921f8b2f999b9ad1c40d9e7f7d
alanne.soares <alannesilva@yahoo.com.br>
2018-07-13 15:26:59 -0300

    alterado

--- ReadDataPDFTable/src/com/company/ReadDataPDFTable.java
+++ ReadDataPDFTable/src/com/company/ReadDataPDFTable.java
@@ -1,65 +1,75 @@
 package com.company;
 
-// Ler pdf com PDFBox
-// Modificado
+// Ler pdf com iText
 
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.parser.PdfTextExtractor;
-import org.apache.pdfbox.cos.*;
-import org.apache.pdfbox.pdfparser.*;
-import org.apache.pdfbox.pdmodel.*;
-import org.apache.pdfbox.util.*;
+import java.sql.*;
+import java.util.*;
+import com.itextpdf.text.pdf.*;
+import com.itextpdf.text.pdf.parser.*;
 
 public class ReadDataPDFTable {
 
     public static void getText() throws IOException {
 
-        PDFParser parser;
-        PDDocument pdDoc;
-        COSDocument cosDoc;
-        PDFTextStripper pdfStripper;
-        String text;
-        String fileName;
-        String[] linhas;
-        String conteudo;
-        File file;
+        String file;
+        String pages;
+        String driver;
+        String url;
+        String username;
+        String password;
 
-        //fileName = "c:/users/alanne.soares/documents/produtividade-juizes-mai-18.pdf";
-        fileName = "c:/users/alann/documents/produtividade-juizes-mai-18.pdf";
-        file = new File(fileName);
+        List<String> linhas;
+
+        int i;
+        //int j;
+
+        file = "c:/users/alanne.soares/documents/produtividade-juizes-mai-18.pdf";
+        //file = "c:/users/alann/documents/produtividade-juizes-mai-18.pdf";
+
+        PdfReader reader = new PdfReader(file);
 
         try {
 
-            parser = new PDFParser(new FileInputStream(file));
-            parser.parse();
-            cosDoc = parser.getDocument();
-            pdfStripper = new PDFTextStripper();
-            pdDoc = new PDDocument(cosDoc);
-            text = pdfStripper.getText(pdDoc);
-            linhas = text.split("\\r?\\n");
+            /*for (i = 1; i < 1000; i++) {
 
-            for (int i = 0; i < linhas.length; i++) {
+                pages = PdfTextExtractor.getTextFromPage(reader, i);
+                linhas = Arrays.asList(pages);//.split("\n"));
 
-                //if (Character.isDigit(linhas[i].charAt(linhas[i].length()) - 1)) {
+                //for (j = 0; j < linhas.get(j).length(); j++) {
 
-                    conteudo = Remove.words(Remove.months(Remove.numbers(Remove.specialCharacteres(linhas[i]))));
+                    //String conteudo = Remove.words(Remove.months(Remove.numbers(Remove.specialCharacteres(linhas.get(i)))));
+                    String conteudo = Remove.words(Remove.months(Remove.numbers(Remove.specialCharacteres(linhas.toString().trim()))));
+*/
+            String atualizaMagistrado = "{ CALL tjrj.tjrj_pa_portal.pr_atualiza_magistrado(?) }";
+            driver = "oracle.jdbc.driver.OracleDriver";
+            url = "jdbc:oracle:thin:@(DESCRIPTION=(LOAD_BALANCE=on)(ADDRESS = (PROTOCOL=TCP)(HOST=exa-scan.pgj.rj.gov.br)(PORT=1521))(CONNECT_DATA = (SERVICE_NAME=CORR)))";
+            username = "TJRJ_WEBSERVICE_CON";
+            password = "TJRJ_WEBSERVICE_CON";
 
-                    if (!conteudo.trim().equals("")) {
-                        System.out.println(String.format(conteudo));
-                    }
-                }
-            //}
+            Class.forName(driver);
 
-        } catch (Exception e) {
+            Connection connection = DriverManager.getConnection(url, username, password);
 
-            System.out.println("Tente novamente!");
+            PreparedStatement ps = connection.prepareStatement(atualizaMagistrado);
+            //ResultSet rs = atualizaMagistrado.executeQuery();
 
+            System.out.println("Conectado!");
+
+                    /*if (!conteudo.isEmpty()) {
+                        if (conteudo.equals("\n")){
+
+                            System.out.println(conteudo.replace("\n", ""));
+
+                        }
+
+                    }*/
+
+
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (SQLException e1) {
+            e1.printStackTrace();
         }
     }
 }
