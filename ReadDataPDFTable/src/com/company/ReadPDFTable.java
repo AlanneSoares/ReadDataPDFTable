a432eae9cbd8cf129ed26a2a5ba2095eea5d3a84
Alanne Soares <alannesilva@yahoo.com.br>
2020-04-05 19:43:21 -0300

    Update ReadPDFTable.java

--- ReadDataPDFTable/src/com/company/ReadPDFTable.java
+++ ReadDataPDFTable/src/com/company/ReadPDFTable.java
@@ -4,7 +4,6 @@ import org.apache.pdfbox.cos.COSDocument;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.util.PDFTextStripper;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Arrays;
@@ -12,42 +11,29 @@ import java.util.List;
 
 
 public class ReadPDFTable {
-
     public static String getText() {
 
         PDFParser parser;
         PDDocument pdDoc;
         COSDocument cosDoc;
         PDFTextStripper pdfStripper;
-
         String textoPdf;
         String fileName = "C://Users//alanne.soares//Documents//produtividade-juizes-mai-18.pdf";
         File file = new File(fileName);
 
         try {
-
             parser = new PDFParser(new FileInputStream(file));
             parser.parse();
             cosDoc = parser.getDocument();
             pdfStripper = new PDFTextStripper();
             pdDoc = new PDDocument(cosDoc);
             textoPdf = pdfStripper.getText(pdDoc);
-
             List<String> map = ChamaLista.removePalavrasInuteis(Arrays.asList(CaracterEspecial.deleteCaracter(textoPdf).split(" ")));
-
             System.out.println(map + CaracterEspecial.deleteCaracter(textoPdf));
-
         } catch (Exception e) {
             e.printStackTrace();
-
             System.out.println("Tente novamente!");
         }
-
         return null;
-
     }
-
 }
-
-
-
